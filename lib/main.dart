06b1805befe444aeaa47420e6cbced515af52397
matheusasc <matheus.assuncao994@gmail.com>
2023-08-08 12:01:19 -0400

    Navegação entre telas de login e home inicial criada, com validação de campos de login e módulos na home adicionados

--- lib/main.dart
+++ lib/main.dart
@@ -7,14 +7,27 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final customColor = MaterialColor(0xFF006633, {
+      50: Color(0xFF006633),
+      100: Color(0xFF006633),
+      200: Color(0xFF006633),
+      300: Color(0xFF006633),
+      400: Color(0xFF006633),
+      500: Color(0xFF006633),
+      600: Color(0xFF006633),
+      700: Color(0xFF006633),
+      800: Color(0xFF006633),
+      900: Color(0xFF006633),
+    });
     return MaterialApp(
       title: 'PMAM',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.teal,
+        primarySwatch: customColor,
       ),
       home: Scaffold(
         appBar: AppBar(
@@ -63,25 +76,37 @@ class MyCustomFormState extends State<MyCustomForm> {
         children: [
           Padding(
             padding: const EdgeInsets.only(bottom: 50),
-            child: Image.asset('assets/images/img.png', width: 300, height: 100),
+            child: Image.asset('assets/images/logo.png', width: 300, height: 100),
           ),
 
           Container(
             width: 300,
             padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-            child: TextField(
+            child: TextFormField(
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Insira seu CPF';
+                }
+                return null;
+              },
               decoration: InputDecoration(
                 border: OutlineInputBorder(),
                 labelText: 'CPF',
                 prefixIcon: Icon(Icons.person),
-              )
+              ),
             ),
           ),
 
           Container(
             width: 300,
             padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-            child: TextField(
+            child: TextFormField(
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Insira sua senha';
+                }
+                return null;
+              },
               decoration: InputDecoration(
                 border: OutlineInputBorder(),
                 labelText: 'Senha',
@@ -102,10 +127,13 @@ class MyCustomFormState extends State<MyCustomForm> {
                     ScaffoldMessenger.of(context).showSnackBar(
                       const SnackBar(content: Text('Carregando...')),
                     );
+                    Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(builder: (context) => HomePage()),
+                    );
                   }
                 },
                 style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.teal,
+                  backgroundColor: Color(0xFF006633),
                   padding: EdgeInsets.symmetric(vertical: 20, horizontal: 130),
                 ),
                 child: const Text('Entrar'),
@@ -137,6 +165,185 @@ class MyCustomFormState extends State<MyCustomForm> {
           ),
         ],
       ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+          actions: <Widget>[
+            IconButton(
+              icon:
+                  Image.asset('assets/icons/configuracoes.png'),
+              tooltip: 'Show Snackbar',
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                    content: Text('This is a Appbar Icon example')));
+              },
+            ),
+          ]
+      ),
+      drawer: Drawer(
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.teal,
+              ),
+              child: Text('Drawer Header'),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.home,
+              ),
+              title: const Text('Page 1'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.train,
+              ),
+              title: const Text('Page 2'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+      body: Container(child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: GridView(
+          children: [
+          InkWell(
+            onTap: (){
+              Navigator.push(context, MaterialPageRoute(builder: (context)=>HomePage()));
+            },
+            child: Container(decoration: BoxDecoration(borderRadius: BorderRadius.circular(10),),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.cloud_done_rounded,size: 50,color: Colors.black,),
+                  Text("Atestado de Antecedentes", textAlign: TextAlign.center, style: TextStyle(color: Colors.black,fontSize: 20),)
+                ],),
+            ),
+          ),
+          InkWell(
+            onTap: (){
+              Navigator.push(context, MaterialPageRoute(builder: (context)=>HomePage()));
+            },
+            child: Container(decoration: BoxDecoration(borderRadius: BorderRadius.circular(10),),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset('assets/icons/pdf.png', color: Colors.black, width: 50, height: 50,),
+                  Text("Boletim Geral",style: TextStyle(color: Colors.black,fontSize: 20),)
+                ],),
+            ),
+          ),
+          InkWell(
+            onTap: (){
+              Navigator.push(context, MaterialPageRoute(builder: (context)=>HomePage()));
+            },
+            child: Container(decoration: BoxDecoration(borderRadius: BorderRadius.circular(10),),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset('assets/icons/id-card.png', color: Colors.black, width: 50, height: 50,),
+                  Text("Carteira de Identidade", textAlign: TextAlign.center, style: TextStyle(color: Colors.black,fontSize: 20),)
+                ],),
+            ),
+          ),
+          InkWell(
+            onTap: (){
+              Navigator.push(context, MaterialPageRoute(builder: (context)=>HomePage()));
+            },
+            child: Container(decoration: BoxDecoration(borderRadius: BorderRadius.circular(10),),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset('assets/icons/sispmam.png',width: 50, height: 50,),
+                  Text("SISPMAM",style: TextStyle(color: Colors.black,fontSize: 20),)
+                ],),
+            ),
+          ),
+            InkWell(
+              onTap: (){
+                Navigator.push(context, MaterialPageRoute(builder: (context)=>HomePage()));
+              },
+              child: Container(decoration: BoxDecoration(borderRadius: BorderRadius.circular(10),),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.folder,size: 50,color: Colors.black,),
+                    Text("Documentos",style: TextStyle(color: Colors.black,fontSize: 20),)
+                  ],),
+              ),
+            ),
+            InkWell(
+              onTap: (){
+                Navigator.push(context, MaterialPageRoute(builder: (context)=>HomePage()));
+              },
+              child: Container(decoration: BoxDecoration(borderRadius: BorderRadius.circular(10),),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset('assets/icons/gun.png', color: Colors.black, width: 50, height: 50,),
+                    Text("CCAF",style: TextStyle(color: Colors.black,fontSize: 20),)
+                  ],),
+              ),
+            ),
+            InkWell(
+              onTap: (){
+                Navigator.push(context, MaterialPageRoute(builder: (context)=>HomePage()));
+              },
+              child: Container(decoration: BoxDecoration(borderRadius: BorderRadius.circular(10),),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.book,size: 50,color: Colors.black,),
+                    Text("SISPMAM",style: TextStyle(color: Colors.black,fontSize: 20),)
+                  ],),
+              ),
+            ),
+            InkWell(
+              onTap: (){
+                Navigator.push(context, MaterialPageRoute(builder: (context)=>HomePage()));
+              },
+              child: Container(decoration: BoxDecoration(borderRadius: BorderRadius.circular(10),),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.book,size: 50,color: Colors.black,),
+                    Text("SISPMAM",style: TextStyle(color: Colors.black,fontSize: 20),)
+                  ],),
+              ),
+            ),
+            InkWell(
+              onTap: (){
+                Navigator.push(context, MaterialPageRoute(builder: (context)=>HomePage()));
+              },
+              child: Container(decoration: BoxDecoration(borderRadius: BorderRadius.circular(10),),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.book,size: 50,color: Colors.black,),
+                    Text("SISPMAM",style: TextStyle(color: Colors.black,fontSize: 20),)
+                  ],),
+              ),
+            ),
+        ],
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3,mainAxisSpacing: 10,crossAxisSpacing: 10),
+        ),
+      ),),
     );
   }
 }
